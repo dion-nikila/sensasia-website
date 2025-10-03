9da7879efc948f297dd321e44fcd0b1816d69d7d
dion-nikila <dionnikila330@gmail.com>
2025-10-03 16:21:37 +0530

    updated menu

--- src/Menu.js
+++ src/Menu.js
@@ -15,41 +15,41 @@ export default function Menu() {
         {/* Food Menu */}
         <div className="menu-card">
           <div className="menu-header">
-            <h3><FaUtensils style={{ marginRight: "0.5rem" }} /> Food Menu</h3>
+            <h3>
+              <FaUtensils style={{ marginRight: "0.5rem" }} /> Food Menu
+            </h3>
           </div>
-          <object
-            data="/FoodMenu.pdf"
-            type="application/pdf"
-            width="100%"
-            height="600px"
-          >
-            <p>
-              You can view our Food menu{" "}
-              <a href="/FoodMenu.pdf" target="_blank" rel="noreferrer">
-                here
-              </a>.
-            </p>
-          </object>
+          <p>
+            View our food menu{" "}
+            <a
+              href="/FoodMenu.pdf"
+              target="_blank"
+              rel="noreferrer"
+              className="menu-link"
+            >
+              here
+            </a>.
+          </p>
         </div>
 
         {/* Drinks Menu */}
         <div className="menu-card">
           <div className="menu-header">
-            <h3><FaCocktail style={{ marginRight: "0.5rem" }} /> Drinks Menu</h3>
+            <h3>
+              <FaCocktail style={{ marginRight: "0.5rem" }} /> Drinks Menu
+            </h3>
           </div>
-          <object
-            data="/DrinksMenu.pdf"
-            type="application/pdf"
-            width="100%"
-            height="600px"
-          >
-            <p>
-              You can view our Drinks menu{" "}
-              <a href="/DrinksMenu.pdf" target="_blank" rel="noreferrer">
-                here
-              </a>.
-            </p>
-          </object>
+          <p>
+            View our drinks menu{" "}
+            <a
+              href="/DrinksMenu.pdf"
+              target="_blank"
+              rel="noreferrer"
+              className="menu-link"
+            >
+              here
+            </a>.
+          </p>
         </div>
       </div>
     </section>
